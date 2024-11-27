9f67b31aad892b8b8df0326bab82ea98d290af2c
Shubham Thakur <shubham.thakur@veersatech.com>
2024-11-27 12:35:59 +0530

    Added dockerfile

--- stayfinder-backend/src/main/java/com/hoteldev/StayFinder/security/SecurityConfig.java
+++ stayfinder-backend/src/main/java/com/hoteldev/StayFinder/security/SecurityConfig.java
@@ -33,7 +33,7 @@ public class SecurityConfig {
         httpSecurity.csrf(AbstractHttpConfigurer::disable)
                 .cors(Customizer.withDefaults())
                 .authorizeHttpRequests(request -> request
-                        .requestMatchers("/auth/**", "/rooms/**","/bookings/**" ).permitAll()
+                        .requestMatchers("/auth/**", "/rooms/**","/bookings/**","/swagger-ui/**", "/v3/api-docs/**","/swagger-ui.html" ).permitAll()
                         .anyRequest().authenticated())
                 .sessionManagement(manager-> manager.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authenticationProvider(authenticationProvider())
